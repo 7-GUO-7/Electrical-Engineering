fea07efb747f84efa550ca8d1ab4569c3eca6225
7 <342718505@qq.com>
2020-02-08 19:46:51 +0800

    Add files via upload

---
+++ 10/lab10.1_color.py
@@ -0,0 +1,58 @@
+import cv2
+import matplotlib.pyplot as plt
+img=cv2.imread("images/img1.png",cv2.IMREAD_COLOR)
+# B G R row before column row1 row2 row3...
+
+# print img[:,:,0]
+B=img[:, :, 0].ravel()
+G=img[:, :, 1].ravel()
+R=img[:, :, 2].ravel()
+sum_B=0
+sum_G=0
+sum_R=0
+for b1 in B:
+    sum_B+=b1
+for g1 in G:
+    sum_G+=g1
+for r1 in R:
+    sum_R+=r1
+sum_all=sum_B+sum_G+sum_R
+sum_B/=float(sum_all)
+sum_G/=float(sum_all)
+sum_R/=float(sum_all)
+RGB_LIST=[sum_B,sum_G,sum_R]
+# print RGB_LIST
+# Xtips=['B','G','R']
+
+# # plt.figure(figsize=(10,30))
+# plt.bar(range(3),RGB_LIST,align="center",color=['B','G','R'],label=['B','G','R'])
+# # plt.hist(range(3),RGB_LIST,align="center",color=['B','G','R'],label=['B','G','R'])
+# plt.xticks(range(3),Xtips)
+# for xx, yy in zip(range(3),RGB_LIST):
+#
+#     plt.text(xx, yy, str(yy), ha='center')
+
+# plt.show()
+
+
+
+plt.figure(figsize=(15, 5))
+ax1 = plt.subplot(131)
+# sublot(a,b,c) divide into a*b and it is the cth picture
+
+ax1.hist(img[:, :, 0].ravel(), density=1,bins=50, color='b')
+ax1.set_title("B")
+# use ravel() to decrease the dimension
+# connect each 2-dim vector to a 1-dim vector
+ax2 = plt.subplot(132)
+ax2.hist(img[:, :, 1].ravel(), density=1,bins=50, color='g')
+ax2.set_title("G")
+
+ax3 = plt.subplot(133)
+ax3.hist(img[:, :, 2].ravel(),density=1, bins=50, color='r')
+ax3.set_title("R")
+plt.show()
+
+# cv2.namedWindow("Image")
+# cv2.imshow("Image",img)
+# cv2.waitKey(0)
---
+++ 10/lab10.2.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy
+import matplotlib.pyplot as plt
+img=cv2.imread("images/img1.png",cv2.IMREAD_GRAYSCALE)
+gray=img.ravel()
+# use ravel() to decrease the dimension
+
+
+plt.hist(gray, bins=100,density=1,color='black')
+# plt.hist(gray, bins=100,normed=1,color='black')
+plt.title("Gray picture of img1")
+plt.show()
+
+# cv2.namedWindow("Image")
+# cv2.imshow("Image",img)
+# cv2.waitKey(0)
+
+# y=len(img)
+# x=len(img[0])
+# gradient={}
+# for t in range (361):
+#     gradient[t]=0
+# for k1 in range(1,x-1):
+#     for k2 in range(1,y-1):
+#         tmp=int(numpy.sqrt(pow(float(img[k2+1][k1])-float(img[k2-1][k1]),2)
+#                            +pow(float(img[k2][k1+1])-float(img[k2][k1-1]),2)))
+#         gradient[tmp]+=1
+#
+# gradient_list=[]
+# sum=0
+# for t in range (361):
+#     gradient_list.append(gradient[t])
+#     sum+=gradient[t]
+# for t in range (361):
+#     gradient_list[t]/=float(sum)
+#
+# plt.bar(range(361),gradient_list,align="center",color="black")
+# plt.title("Gragient of img2")
+# plt.show()
